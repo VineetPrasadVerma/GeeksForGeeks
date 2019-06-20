60079340dc3effda1f4f572d0fd6f9f803b1ab3d
Vineet Prasad Verma <vineetpd1996@gmail.com>
2019-06-21 01:16:12 +0530

    Update AbsoluteDifferenceOfOne

--- SudoPlacementCourse/AbsoluteDifferenceOfOne.py
+++ SudoPlacementCourse/AbsoluteDifferenceOfOne.py
@@ -1,31 +1,31 @@
-    test_cases = int(input())
-    for _ in range(test_cases):
-        size, num = tuple(map(int, input().split()))
-        arr_list = list(map(int, input().split()))
-        reduced_list = []
-        final_list = []
-        for i in arr_list:
-            if i < num:
-                reduced_list.append(i)
+test_cases = int(input())
+for _ in range(test_cases):
+    size, num = tuple(map(int, input().split()))
+    arr_list = list(map(int, input().split()))
+    reduced_list = []
+    final_list = []
+    for i in arr_list:
+        if i < num:
+            reduced_list.append(i)
 
-        for i in reduced_list:
-            temp = i
-            prev_digit = -1
-            while temp:
-                curr_digit = temp % 10
-                if prev_digit == -1:
+    for i in reduced_list:
+        temp = i
+        prev_digit = -1
+        while temp:
+            curr_digit = temp % 10
+            if prev_digit == -1:
+                prev_digit = curr_digit
+            else:
+                if abs(prev_digit - curr_digit) == 1:
                     prev_digit = curr_digit
+                    if temp == int(str(i)[0]):
+                        final_list.append(i)
                 else:
-                    if abs(prev_digit - curr_digit) == 1:
-                        prev_digit = curr_digit
-                        if temp == int(str(i)[0]):
-                            final_list.append(i)
-                    else:
-                        break
+                    break
 
-                temp = int(temp/10)
-        if len(final_list) == 0:
-            print(-1)
-        else:
-            print(" ".join(str(i) for i in final_list))
+            temp = int(temp/10)
+    if len(final_list) == 0:
+        print(-1)
+    else:
+        print(" ".join(str(i) for i in final_list))
 
