c6f52b93b98fa05603e7d463f2eb2cdf2b72d0d7
Vineet Prasad Verma <vineetpd1996@gmail.com>
2019-10-22 22:53:31 +0530

    Solved Possible words from phone digits

--- Revision/PossibleWordsFromPhoneDigits.py
+++ Revision/PossibleWordsFromPhoneDigits.py
@@ -1,8 +1,27 @@
-n = int(input())
-for _ in range(n):
-    size, window = tuple(map(int, input().split()))
-    arr = list(map(int, input().split()))
-    sub_lists = []
-    for i in range(0, size, window):
-        sub_lists.append(arr[i: i + window])
-    print(sub_lists)
+import itertools
+test_cases = int(input())
+for i in range(0, test_cases):
+    no_of_digits = int(input())
+    list_of_digits = input()
+    int_list_of_digits = list(map(int, list_of_digits.split()))
+    str_list_of_digits = []
+    dicts = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqrs',
+        8: 'tuv',
+        9: 'wxyz'
+    }
+
+    for i in int_list_of_digits:
+        str_list_of_digits.append(dicts[i])
+
+    ans_list = itertools.product(* str_list_of_digits)
+
+    for first in ans_list:
+        for j in first:
+            print(j, end="")
+        print(end=" ")
