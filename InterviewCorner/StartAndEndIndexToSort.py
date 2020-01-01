53428cef86ae94208a84e1b57065799c7c8ffe33
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-01-01 08:59:29 +0530

    Corrected the solution

--- InterviewCorner/StartAndEndIndexToSort.py
+++ InterviewCorner/StartAndEndIndexToSort.py
@@ -11,8 +11,19 @@ def func(arr):
         if arr[i] < arr[i-1]:
             end_index = i
             break
-
+    temp = arr[start_index: end_index+1]
+    mini = min(temp)
+    maxi = max(temp)
+    for i in range(start_index):
+        if arr[i] > mini:
+            start_index = i
+            break
+    for i in range(length-1, end_index-1, -1):
+        if arr[i] < maxi:
+            end_index = i
+            break
     print(start_index, end_index)
 
 
-func([10, 3, 2, 5, 4, 6])
+func([10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60])
+func([0, 1, 15, 25, 6, 7, 30, 40, 50])
